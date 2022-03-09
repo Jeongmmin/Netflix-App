30edfa00eb36049aa93a3bc0632b20a5c999141a
Jeongmmin <seo88kr@hanmail.net>
2022-03-09 19:43:57 +0900

    add. vidio-banner

--- src/api/requests.js
+++ src/api/requests.js
@@ -5,9 +5,11 @@ const requests = {
     fetchTopRated: `/movie/top_rated`,
     fetchActionMovies: `/discover/movie?with_geners=28`,
     fetchComedyMovies: `/discover/movie?with_geners=35`,
-    fetchHorrorMovies: `/discover/movie?with_geners=27`,
+    // fetchHorrorMovies: `/discover/movie?with_geners=27`,
     fetchRomanceMovies: `/discover/movie?with_geners=10749`,
     fetchDocumentaries: `/discover/movie?with_geners=99`,
+    fetchMystery: "/discover/movie?with_genres=9648",
+    fetchDrama: "/discover/movie?with_genres=18",
 }
 export default requests;
 
