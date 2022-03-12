92d4a3d8b899522142eefece6699fb25d04b9a4f
Jeongmmin <seo88kr@hanmail.net>
2022-03-13 02:48:44 +0900

    add Row Components, Slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './api/requests';
 
 function App() {
   return (
@@ -8,6 +10,43 @@ function App() {
       <Nav/>
 
       <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        id="TN"
+        fetchUrl={requests.fetchTrending}
+      />
+      <Row
+        title="Top Rated"
+        id="TR"
+        fetchUrl={requests.fetchTopRated}
+      />
+      <Row
+        title="Music"
+        id="MS"
+        fetchUrl={requests.fetchMusic}
+      />
+      <Row
+        title="Romance Movies"
+        id="RM"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Mistery"
+        id="MT"
+        fetchUrl={requests.fetchMystery}
+      />
     </div>
   );
 }
--- src/api/requests.js
+++ src/api/requests.js
@@ -3,13 +3,14 @@ const requests = {
     fetchNetflixOriginals: `/discover/tv?with_networks=213`,
     fetchTrending: `/trending/all/week`,
     fetchTopRated: `/movie/top_rated`,
-    fetchActionMovies: `/discover/movie?with_geners=28`,
-    fetchComedyMovies: `/discover/movie?with_geners=35`,
-    // fetchHorrorMovies: `/discover/movie?with_geners=27`,
-    fetchRomanceMovies: `/discover/movie?with_geners=10749`,
-    fetchDocumentaries: `/discover/movie?with_geners=99`,
+    fetchActionMovies: `/discover/movie?with_genres=28`,
+    fetchComedyMovies: `/discover/movie?with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?with_genres=99`,
     fetchMystery: "/discover/movie?with_genres=9648",
     fetchDrama: "/discover/movie?with_genres=18",
+    fetchMusic: "/discover/movie?with_genres=10402",
 }
 export default requests;
 
