816bc270791d8fdd20b9bcdd7a528ac8eb96541d
Jeongmmin <seo88kr@hanmail.net>
2022-04-24 19:03:42 +0900

    git test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import { Outlet, Route, Routes } from 'react-router-dom';
-import MainPage from './Pages/MainPage';
-import DetailPage from './Pages/DetailPage';
-import SearchPage from './Pages/SearchPage';
+import "./App.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { Outlet, Route, Routes } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
+import DetailPage from "./Pages/DetailPage";
+import SearchPage from "./Pages/SearchPage";
 
 // 라우팅을 위한 파일로 만들기
 const Layout = () => {
@@ -12,26 +12,23 @@ const Layout = () => {
     <div className="wrapper">
       <Nav />
 
-
       <Outlet />
 
-
       <Footer />
-
     </div>
-  )
-}
+  );
+};
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<Layout />} >
-            <Route index element={<MainPage />}/>
-            <Route path=':movieId' element={<DetailPage />}/>
-            <Route path='search' element={<SearchPage />}/>
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
