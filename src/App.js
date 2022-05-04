c0d588d03658ecd2d8277416a1ec934a669d1066
Jeongmmin <seo88kr@hanmail.net>
2022-05-04 23:54:18 +0900

    💄 Change styledComponents

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import "./App.css";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import { Outlet, Route, Routes } from "react-router-dom";
 import MainPage from "./Pages/MainPage";
 import DetailPage from "./Pages/DetailPage";
 import SearchPage from "./Pages/SearchPage";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  margin: 0;
+`;
 
 // 라우팅을 위한 파일로 만들기
 const Layout = () => {
   return (
-    <div className="wrapper">
+    <Wrapper>
       <Nav />
 
       <Outlet />
 
       <Footer />
-    </div>
+    </Wrapper>
   );
 };
 
